7b7d720e0e5ba8897bbdcfc06af778388883db8f
Sanjeev Yogi <yogi.sjv@gmail.com>
2020-05-20 01:17:04 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -1,14 +1,14 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-        montague: {},
     matriarch: {},
     hero: {},
     hero_friends: [] 
       }, 
     :capulet => {
       #your key/value pairs here!
+    matriarch: {},
+    hero: {},
+    heroine_friends: [] 
       }}
-
-  
 end
