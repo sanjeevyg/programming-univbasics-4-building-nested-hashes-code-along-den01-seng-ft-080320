00ca2e3138ff381aa8cbf4a138c0cf914e6fd8d4
Sanjeev Yogi <yogi.sjv@gmail.com>
2020-05-20 01:25:24 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -27,7 +27,7 @@ def third_challenge
         },
       :matriarch => {
         #your key/value pairs here
-        name: "Lady Montague", 
+        name: "Lady Capulet", 
         age: "51" 
         },
       :heroine => {
